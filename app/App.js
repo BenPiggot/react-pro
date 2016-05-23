001483eca12eb8bca1a3b474bfe4d2a80375b838
wbp <wbpiggot@gmail.com>
2016-05-22 22:05:38 -0700

    Basic app in place

--- app/App.js
+++ app/App.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import KanbanBoard from './KanbanBoard';
 
-class Hello extends React.Component  {
-  render() {
-    return <h1>Hello World</h1>
+let cardsList = [
+  {
+    id: 1,
+    title: 'Read the Book',
+    description: 'I should read the whole book',
+    status: 'in-progress',
+    tasks: []
+  },
+  {
+    id: 2,
+    title: 'Write some code',
+    description: 'Code along with the samples in the book',
+    status: 'todo',
+    tasks: [
+      {
+        id: 1,
+        name: 'ContactList Example',
+        done: true
+      },
+      {
+        id: 2,
+        name: 'Kanban Example',
+        done: false
+      },
+      {
+        id: 2,
+        name: 'My own experiments',
+        done: false
+      },
+    ]    
   }
-}
 
-React.render(<Hello />, document.getElementById('root'));
+
+]
+
+
+React.render(<KanbanBoard cards={cardsList} />, document.getElementById('root'));
