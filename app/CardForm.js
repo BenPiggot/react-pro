95909830edc5ed2d76f29771a11455e100d6bd3d
wbp <wbpiggot@gmail.com>
2016-06-11 22:05:24 -0700

    wip

--- app/CardForm.js
+++ app/CardForm.js
@@ -15,30 +15,35 @@ class CardForm extends Component {
       <div className='card big'>
         <form onSubmit={ this.props.handleSubmit.bind(this) }>
           <input type='text' 
+                 style={{display: 'block'}}
                  value={this.props.draftCard.title}
                  onChange={this.handleChange.bind(this, 'title')}
                  placeholder='Title'
                  required={true}
                  autoFocus={true} />
           <textarea value={this.props.draftCard.description}
+                 style={{display: 'block'}}
                  onChange={this.handleChange.bind(this, 'description')}
                  placeholder='Description'
                  required={true} />
-          <label htmlFor='status'>Status</label>
-          <select id='status'
-                  value={this.props.draftCard.status}
-                  onChange={this.handleChange.bind(this, 'status')}>
-            <option value='todo'>To Do</option>
-            <option value='in-progress'>In Progress</option>
-            <option value='done'>Done</option>
-          </select>
-          <br />
-          <label htmlFor='color'>Color</label>
-          <input id='color'
-                 value={this.props.draftCard.color}
-                 onChange={this.handleChange.bind(this, 'color')}
-                 type='color'
-                 defaultValue='#ff0000' />
+          <div className='form-group'>
+            <label htmlFor='status'>Status</label>
+            <select id='status'
+                    value={this.props.draftCard.status}
+                    onChange={this.handleChange.bind(this, 'status')}>
+              <option value='todo'>To Do</option>
+              <option value='in-progress'>In Progress</option>
+              <option value='done'>Done</option>
+            </select>
+          </div>
+          <div className='form-group'>
+            <label htmlFor='color'>Color</label>
+            <input id='color'
+                   value={this.props.draftCard.color}
+                   onChange={this.handleChange.bind(this, 'color')}
+                   type='color'
+                   defaultValue='#ff0000' />
+          </div>
 
           <div className='actions'>
             <button type='submit'>{this.props.buttonLabel}</button>
